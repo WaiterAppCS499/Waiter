267ef16e62a3d4be8dae288aa32408b387dfc335
gnomiethelovely <nunuzami@outlook.com>
2015-05-04 19:08:05 -0700

    Added bare bones post and search features.

--- app/src/main/java/com/example/noemi/waiter/PostActivity.java
+++ app/src/main/java/com/example/noemi/waiter/PostActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.parse.ParseObject;
 
 /**
  * Created by Noemi on 4/21/2015.
@@ -16,16 +20,36 @@ public class PostActivity extends Activity{
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.post_layout);
 
+        final EditText rname = (EditText) findViewById(R.id.restaurantname);
+        final EditText city = (EditText) findViewById(R.id.city);
+        final EditText zip = (EditText) findViewById(R.id.zip);
+        final NumberPicker partysize = (NumberPicker) findViewById(R.id.postingsize);
+        final NumberPicker waittime = (NumberPicker) findViewById(R.id.postingtime);
+
         Button button1 = (Button) findViewById(R.id.post);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(PostActivity.this, PostsActivity.class);
-                startActivity(intent);
+                String rn = rname.getText().toString();
+                String ct = city.getText().toString();
+                String z = zip.getText().toString();
+
+                if(rn.equals("") && ct.equals("") && z.equals("")){
+                    Toast.makeText(getApplicationContext(), "All fields required!", Toast.LENGTH_SHORT).show();
+                }else {
+                    ParseObject data = new ParseObject("Waittime");
+                    data.put("name", rn);
+                    data.put("city", ct);
+                    data.put("zipcode", z);
+                    data.put("partysize", partysize.getValue());
+                    data.put("waittime", waittime.getValue());
+                    data.saveInBackground();
+                    Intent intent = new Intent(PostActivity.this, PostsActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
-        NumberPicker partysize = (NumberPicker) findViewById(R.id.postingsize);
 
         partysize.setMinValue(1);
         partysize.setMaxValue(10);
@@ -37,8 +61,6 @@ public class PostActivity extends Activity{
 
             }
         });
-
-        NumberPicker waittime = (NumberPicker) findViewById(R.id.postingtime);
 
         waittime.setMinValue(1);
         waittime.setMaxValue(240);
